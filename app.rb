1d531abebcef8ce2b384e288c8ae17c20735853c
hendrik <g9a2hvk9@me.com>
2017-04-26 23:03:10 +0100

    Adds .gif for player 1 and 2 as well as CSS formatted background

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require "sinatra/base"
 
 class Battle < Sinatra::Base
-  set :session_secret, "This should keep shotgun in line"
+  set :session_secret, "This should make sessions work in shotgun"
   enable :sessions
 
   get "/" do
@@ -12,13 +12,25 @@ class Battle < Sinatra::Base
   post "/names" do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
+    session[:hp_player1] = rand(21)+80
+    session[:hp_player2] = rand(21)+80
     redirect to('/play')
   end
 
   get "/play" do
     @player1 = session[:player1]
+    @hp_player1 = session[:hp_player1]
     @player2 = session[:player2]
+    @hp_player2 = session[:hp_player2]
     erb(:play)
+  end
+
+  get "/fight" do
+    @player1 = session[:player1]
+    @hp_player1 = session[:hp_player1]
+    @player2 = session[:player2]
+    @hp_player2 = session[:hp_player2]
+    erb(:fight)
   end
 
   run! if app_file == $0
