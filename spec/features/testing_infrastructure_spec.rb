5b4a90db5a190e375e6372d96c276d0bc6291797
hendrik <g9a2hvk9@me.com>
2017-04-26 23:22:41 +0100

    Adds and passes rspec tests for all pages not currently under development

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,14 +8,28 @@ feature 'Testing infrastructure' do
   end
 end
 
-  feature 'Entering player names and displaying them' do
-    scenario "Expects player names to be displayed with a commencement message" do
-      visit('/')
-      fill_in 'player1', :with => 'Widgyoto'
-      fill_in 'player2', :with => 'Gordon'
-      click_button 'Create Players'
-      expect(page).to have_text('Player 1 is Widgyoto')
-      expect(page).to have_text('Player 2 is Gordon')
-      expect(page).to have_text('Let the battle commence!!!')
-    end
+feature 'Entering player names and displaying them' do
+  scenario "Expects player names to be displayed with a commencement message" do
+    visit('/')
+    fill_in 'player1', :with => 'Widgyoto'
+    fill_in 'player2', :with => 'Gordon'
+    click_button 'Create Players'
+    expect(page).to have_text('Widgyoto gets ')
+    expect(page).to have_text('Gordon gets')
+    expect(page).to have_button('FIGHT!')
+  end
+
+feature 'Hitting the FIGHT!' do
+  scenario 'It takes you to a new page' do
+    visit('/')
+    fill_in 'player1', :with => 'Widgyoto'
+    fill_in 'player2', :with => 'Gordon'
+    click_button 'Create Players'
+    click_button 'FIGHT!'
+    save_and_open_page
+    expect(page).to
+  end
+end
+
+
   end
