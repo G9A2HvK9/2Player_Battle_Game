9538ac11296609c0b15f5ec01b33fcdec4341b9c
hendrik <g9a2hvk9@me.com>
2017-04-26 15:55:13 +0100

    Adds display functionality for player names and sessions to /names and /play routes. CAREFUL there is a shotgun workaround 'secret_session' in the configure block

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
-#require "rubygems"
 require "sinatra/base"
-#require "shotgun"
 
 class Battle < Sinatra::Base
+  set :session_secret, "This should keep shotgun in line"
+  enable :sessions
+
   get "/" do
-    "Testing infrastructure working!"
+    erb(:index)
+    # "player1 =" << session[:player1].inspect
+  end
+
+  post "/names" do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect to('/play')
+  end
+
+  get "/play" do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,5 +6,16 @@ feature 'Testing infrastructure' do
     visit('/')
     expect(page).to have_content "Testing infrastructure working!"
   end
-
 end
+
+  feature 'Entering player names and displaying them' do
+    scenario "Expects player names to be displayed with a commencement message" do
+      visit('/')
+      fill_in 'player1', :with => 'Widgyoto'
+      fill_in 'player2', :with => 'Gordon'
+      click_button 'Create Players'
+      expect(page).to have_text('Player 1 is Widgyoto')
+      expect(page).to have_text('Player 2 is Gordon')
+      expect(page).to have_text('Let the battle commence!!!')
+    end
+  end
