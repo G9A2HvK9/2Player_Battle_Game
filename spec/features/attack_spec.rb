fb4e45eba87eae260b5b22cadb923ffd084d29e5
hendrik <g9a2hvk9@me.com>
2017-04-27 15:42:18 +0100

    Adds chicago style spec test to attack_spec.rb and passes

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,3 +8,13 @@ feature "Player 1 can attack Player 2" do
     expect(@hp_player_2).to change_by(-10)
   end
 end
+
+feature "Player 2 can attack Player 1" do
+  scenario "when I click the 'attack' button, it reduces HP from player 1 by 10" do
+    $player1 = Player.new("Fighter",100)
+    $player2 = Player.new("Fighter2",100)
+    visit('/fight')
+    click_button("Attack!")
+    expect($player1.player_hp).to eq(90)
+  end
+end
